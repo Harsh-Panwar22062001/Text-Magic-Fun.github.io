65c10bb2a8ea553053eebe87fa2b4960478921d1
Harsh <knlharshkumar@gmail.com>
2023-03-05 23:09:49 +0530

    reactbasic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name = "Harsh Panwar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>contact</li>
+        <li>About</li>
+      </nav>
+      <div className="container">
+      <h1>Hello {name}</h1>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate assumenda autem dolor itaque cumque iure dolorum ex, rerum ipsum magnam ipsa perspiciatis aliquid velit. Omnis libero commodi sed error repellat?
+  </div>
+    </>
+
+  
+  
   );
 }
 
